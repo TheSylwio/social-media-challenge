311745154d57409b290a2d829d6af5ab45d8a8fd
thesylwio <thesylwio@gmail.com>
2020-08-29 19:24:00 +0200

    ✨ Implemented banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from "./components/Banner";
 import './App.css';
 
 function App() {
   return (
     <div className='app'>
-      TEST
+      <Banner/>
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className='banner'>
+      <div className="banner__info">
+        <h1 className="banner__info--primary">Social Media Dashboard</h1>
+        <h2 className="banner__info--secondary">Total Followers: 23,004</h2>
+      </div>
+      <hr/>
+      <div className="banner__toggle">
+        <label htmlFor="darkModeButton">Dark Mode</label>
+        <div className="toggle__wrapper">
+          <input id="darkModeButton" className="toggle__button" type="checkbox"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
