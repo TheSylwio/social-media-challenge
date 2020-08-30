da8dbb08cd817d2842e405a7d7e535b1cfe23172
thesylwio <thesylwio@gmail.com>
2020-08-30 22:09:12 +0200

    💳 Card top borders implemented

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,28 @@
 import React from "react";
 import IconDown from '../assets/icon-down.svg';
 import IconUp from '../assets/icon-up.svg';
+import IconFacebook from "../assets/icon-facebook.svg";
+import IconTwitter from "../assets/icon-twitter.svg";
+import IconInstagram from "../assets/icon-instagram.svg";
+import IconYoutube from "../assets/icon-youtube.svg";
 import './Card.css';
 
 function Card(props) {
+  const getSocialMediaStyle = () => {
+    switch (props.icon) {
+      case IconFacebook:
+        return 'card--facebook';
+      case IconTwitter:
+        return 'card--twitter';
+      case IconInstagram:
+        return 'card--instagram ';
+      case IconYoutube:
+        return 'card--youtube';
+    }
+  }
+
   return (
-    <div className='card'>
+    <div className={`card ${getSocialMediaStyle()}`}>
       <div className="card__title">
         <img src={props.icon} alt=""/>
         <span>{props.username}</span>
