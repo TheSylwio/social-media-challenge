850516938733cc425e9cbb90a1e2432f51b199d1
thesylwio <thesylwio@gmail.com>
2020-08-31 09:55:19 +0200

    Updates

---
+++ precache-manifest.eddb60676f4292b1749e5385afba648f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26f2cd42aeadba7c3263e63deb08dd1",
+    "url": "/social-media-challenge/index.html"
+  },
+  {
+    "revision": "cfcfd61b94d832fef64d",
+    "url": "/social-media-challenge/static/css/main.9a1d342b.chunk.css"
+  },
+  {
+    "revision": "1fb8ab21c45c2742b8f1",
+    "url": "/social-media-challenge/static/js/2.38b55739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-challenge/static/js/2.38b55739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcfd61b94d832fef64d",
+    "url": "/social-media-challenge/static/js/main.8f416b8f.chunk.js"
+  },
+  {
+    "revision": "01762bd5dcdbb95682d0",
+    "url": "/social-media-challenge/static/js/runtime-main.50d486dc.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/social-media-challenge/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/social-media-challenge/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/social-media-challenge/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/social-media-challenge/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/social-media-challenge/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/social-media-challenge/static/media/icon-youtube.8499da12.svg"
+  }
+]);
